3eeacd8e74885e7a73c4b0b921ce267bf7f4205d
Luiz Vinicius <viniciusadr244@gmail.com>
2024-11-12 12:22:06 -0300

    refactor: hooks

--- src/hooks/useWeatherForecast.ts
+++ src/hooks/useWeatherForecast.ts
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query"
 
 const getWeatherByCity = async (cityName: string) => {
 	const request = await fetch(
-		`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${process.env.NEXT_PUBLIC_API_KEY}`,
+		`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=metric&lang=pt_br&appid=${process.env.NEXT_PUBLIC_API_KEY}`,
 	)
 	const data = await request.json()
 	return data
