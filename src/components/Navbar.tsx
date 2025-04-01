343dce1a7896d82396d7f246a47e4b54c6cb0f40
Luiz Vinicius <viniciusadr244@gmail.com>
2025-04-01 10:16:18 -0300

    feat: enhance Navbar style adding focus stylization

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,15 +13,27 @@ export default function Navbar() {
 				<Icon
 					icon="fluent:home-24-filled"
 					onClick={() => toggleDrawer("home", false)}
-					className="text-white text-lg cursor-pointer"
+					tabIndex={0}
+					className="h-1/2 w-10 flex justify-center items-center text-white text-lg cursor-pointer focus:border-b focus:border-b-[hsl(237, 25%, 20%)]"
 				/>
 				<Icon
 					icon="mynaui:search"
 					onClick={() => toggleDrawer("search", !value.isOpen)}
-					className="text-white text-lg cursor-pointer"
+					tabIndex={0}
+					className="h-1/2 w-10 flex justify-center items-center text-white text-lg cursor-pointer focus:border-b focus:border-b-[hsl(237, 25%, 20%)]"
 				/>
-				<Icon icon="solar:bell-outline" className="text-white text-lg cursor-pointer" />
-				<Icon icon="proicons:map" className="text-white text-lg cursor-pointer" />
+				<Icon
+					icon="solar:bell-outline"
+					onClick={() => toggleDrawer("notification", false)}
+					tabIndex={0}
+					className="h-1/2 w-10 flex justify-center items-center text-white text-lg cursor-pointer focus:border-b focus:border-b-[hsl(237, 25%, 20%)]"
+				/>
+				<Icon
+					icon="proicons:map"
+					onClick={() => toggleDrawer("map", false)}
+					tabIndex={0}
+					className="h-1/2 w-10 flex justify-center items-center text-white text-lg cursor-pointer focus:border-b focus:border-b-[hsl(237, 25%, 20%)]"
+				/>
 			</div>
 		</div>
 	)
